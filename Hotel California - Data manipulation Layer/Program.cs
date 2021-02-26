bf52a2dacde0dad0d1d89dc09244b3d69616dcec
Martin Useo <martin.useo@gmail.com>
2021-02-26 15:33:23 +0100

    Getting the DB to receive queries from a test C# code

--- Hotel California - Data manipulation Layer/Program.cs
+++ Hotel California - Data manipulation Layer/Program.cs
@@ -12,7 +12,25 @@ namespace Hotel_California___Data_manipulation_Layer
     {
         static void Main(string[] args)
         {
+            Entities_oui dc = new Entities_oui();
 
+            DbSet<Rooms> Rooms = dc.Rooms;
+
+            var orderRooms = Rooms.OrderBy(rooms => rooms.Rooms_ID);
+
+            foreach (Rooms r in orderRooms)
+                Console.WriteLine("Room {0} is size {1}", r.Rooms_ID, r.Size);
+            Console.ReadKey();
+            Console.WriteLine("========================================");
+
+            orderRooms = from rooms in Rooms
+                         orderby rooms.Rooms_ID
+                         select rooms;
+
+            foreach (Rooms r in orderRooms)
+                Console.WriteLine("Room {0} is a {1} room", r.Rooms_ID, r.Quality);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Test/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+<Assembly: AssemblyTitle("Test")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Test")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("a2e0f24d-44bc-43bc-b4c9-e3fea72cc595")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
