33e0a394b4159ff76c22a9a0cf310e18d70057cd
Martin Useo <martin.useo@gmail.com>
2021-03-01 00:18:15 +0100

    Again Blazor Changes

---
+++ Hotel California - WebClient/Client/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hotel_California_-_WebClient.Client.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
