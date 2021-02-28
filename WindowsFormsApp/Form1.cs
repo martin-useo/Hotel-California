2285f0466b7cbfd132991fadcb993fb7f459dfc4
Mirnes <mirnes1997@hotmail.com>
2021-02-28 13:25:45 +0100

    Added interface

---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CreateReservation() { }
+
+        public String ReadReservation() { return ""; }
+
+        public void UpdateReservation() { }
+
+        public void DeleteReservation() { }
+
+        public void RegisterRoomService() { }
+
+        public void RegisterMaintenanceRequest() { }
+
+
+
+    }
+}
