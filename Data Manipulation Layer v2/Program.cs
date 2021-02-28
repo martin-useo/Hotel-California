4707cb3b054609930eb0694ba917e73446e02629
Martin Useo <martin.useo@gmail.com>
2021-02-28 12:16:55 +0100

    Cleaning things up

---
+++ Data Manipulation Layer v2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Data_Manipulation_Layer_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
