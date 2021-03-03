6be55590bcca02f5ceeb88d83479eadb6d627ed8
Mirnes <mirnes1997@hotmail.com>
2021-03-03 18:09:43 +0100

    Improvement to datalayer

--- Hotel California - Data manipulation Layer/DataLayer.cs
+++ Hotel California - Data manipulation Layer/DataLayer.cs
@@ -32,6 +32,7 @@ namespace Hotel_California___Data_manipulation_Layer
         }
         public void Add_Room(int rid, int rpplcnt, string rqlt, string rsize)
         {
+            // Uses parameters to create a room object of the room that should be added
             Rooms nr = new Rooms
             {
                 Rooms_ID = rid,
@@ -40,6 +41,7 @@ namespace Hotel_California___Data_manipulation_Layer
                 Size = rsize
             };
 
+            // Check if room with that roomid / room number already exists
             List<Rooms> rooms = Get_All_Rooms();
             bool pkTaken = false;
             foreach (Rooms r in rooms)
@@ -50,6 +52,7 @@ namespace Hotel_California___Data_manipulation_Layer
                 }
             }
 
+            // If there are no room with that room id / room number, the new room will be added
             if (!pkTaken)
             {
                 dc.Rooms.Add(nr);
@@ -84,24 +87,28 @@ namespace Hotel_California___Data_manipulation_Layer
 
             return rooms;
         }
-        public void Add_Reservation( int rid, string cname, string cpassword, DateTime begins, DateTime ends)
+        public void Add_Reservation(int rid, string cname, string cpassword, DateTime begins, DateTime ends)
         {
             Booked_Rooms br = new Booked_Rooms();
             br.Clients_ID = Login(cname, cpassword);
+
+            if (br.Clients_ID == -1 || (0 < DateTime.Compare(begins, ends))){}  // If client login fails or dates don't make sense: Do not create reservation
+            else
+            {
             br.Rooms_ID = rid;
             br.Begins = begins;
             br.Ends = ends;
 
             int reservationId = 0;
-            List<Booked_Rooms> reservations = Get_All_Reservations();
             bool pkTaken = false;
+            List<Booked_Rooms> reservations = Get_All_Reservations();
             foreach (Booked_Rooms r in reservations)
             {
-                if (r.Reservation_ID == br.Reservation_ID)
+                if (r.Rooms_ID == br.Rooms_ID)  // If room is already reserved
                 {
                     pkTaken = true;
                 }
-                if (r.Reservation_ID > br.Reservation_ID)
+                if (r.Reservation_ID >= br.Reservation_ID)   // Get new unique number for reservation id
                 {
                     reservationId = r.Reservation_ID + 1;
                 }
@@ -111,7 +118,7 @@ namespace Hotel_California___Data_manipulation_Layer
 
             List<Rooms> rooms = Get_All_Rooms();
             bool roomExists = false;
-            foreach (Rooms r in rooms)
+            foreach (Rooms r in rooms)      // To create reservation a valid room must be entered
             {
                 if (r.Rooms_ID == br.Rooms_ID)
                 {
@@ -119,13 +126,13 @@ namespace Hotel_California___Data_manipulation_Layer
                 }
             }
 
-            if (!pkTaken && roomExists)
+            if (!pkTaken && roomExists)     // Only add if room exists and is available
             {
                 dc.Booked_Rooms.Add(br);
                 dc.SaveChanges();
             }
         }
-
+        }
         public void Del_Reservation( int bid)
         {
             Booked_Rooms bdel = dc.Booked_Rooms.Where(rm => rm.Reservation_ID == bid).FirstOrDefault();
@@ -139,23 +146,25 @@ namespace Hotel_California___Data_manipulation_Layer
         // TODO
         public List<Booked_Rooms> Get_All_Reservations()
         {
-            List<Booked_Rooms> rooms = new List<Booked_Rooms>();
+            List<Booked_Rooms> reservationList = new List<Booked_Rooms>();
+            var roomSet = dc.Booked_Rooms.OrderBy(r => r.Reservation_ID);
+            foreach (Booked_Rooms r in roomSet)
+                reservationList.Add(r);
 
-
-            return rooms;
+            return reservationList;
         }
 
         public void Add_Client( string cname, string cpassword)
         {
             Clients ncl = new Clients();
 
-            Clients clientHighestId = dc.Clients.OrderBy(x => x.Clients_ID).FirstOrDefault();
+            Clients clientHighestId = dc.Clients.OrderByDescending(x => x.Clients_ID).FirstOrDefault();
             ncl.Clients_ID = clientHighestId.Clients_ID + 1;
             ncl.Name = cname;
             ncl.Password = cpassword;
 
             // TODO check if client already exists before adding
-            Clients client = dc.Clients.Where(gid => gid.Name == cname).Where(gid => gid.Password == cpassword).FirstOrDefault();
+            Clients client = dc.Clients.Where(gid => gid.Name == cname && gid.Password == cpassword).FirstOrDefault();
             if (client == null)
             {
             dc.Clients.Add(ncl);
@@ -237,8 +246,7 @@ namespace Hotel_California___Data_manipulation_Layer
 
             foreach (Rooms r0 in RoomsList)
                 Console.WriteLine("{0} {1} {2} {3}", r0.Rooms_ID, r0.People_Count, r0.Quality, r0.Size);
-            Console.WriteLine("=====================================");
-            Console.ReadKey();
+            Console.WriteLine("=====================================" + "\n" + "\n" + "\n");
         }
         public void Disp_Tasks()
         {
@@ -248,7 +256,7 @@ namespace Hotel_California___Data_manipulation_Layer
             Console.WriteLine("id       type           status");
             foreach (Tasks t in Tasks)
                 Console.WriteLine("{0}{1}{2}");
-            Console.ReadKey();
+            Console.WriteLine("=====================================" + "\n" + "\n" + "\n");
         }
         public void Disp_Clients()
         {
@@ -259,14 +267,22 @@ namespace Hotel_California___Data_manipulation_Layer
             Console.WriteLine("id                name             password");
             foreach (Clients c0 in ClientsList)
                 Console.WriteLine("{0} {1} {2}", c0.Clients_ID, c0.Name, c0.Password);
-            Console.WriteLine("======================================");
-            Console.ReadKey();
+            Console.WriteLine("=====================================" + "\n" + "\n" + "\n");
         }
 
+        public void Disp_Reservations()
+        {
+            var reservationList = dc.Booked_Rooms.OrderBy(r => r.Reservation_ID);
+
+            Console.WriteLine("Reservation ID           Rooms id      Client Id      Date Begins     Date ends");
+            foreach (Booked_Rooms r in reservationList)
+                Console.WriteLine("{0} {1} {2} {3} {4}", r.Reservation_ID, r.Rooms_ID, r.Clients_ID, r.Begins, r.Ends);
+            Console.WriteLine("=====================================" + "\n" + "\n" + "\n");
+        }
 
         public int Login( string cname, string cpassword)
         {
-            int cid = 0;
+            int cid = -1;
 
             Clients client = dc.Clients.Where(gid => gid.Name == cname).Where(gid => gid.Password == cpassword).FirstOrDefault();
 
@@ -286,22 +302,23 @@ namespace Hotel_California___Data_manipulation_Layer
 
         static void Main()
         {
-            /*
+
             DataLayer dl = new DataLayer();
 
             //  /!\TESTING ZONE/!\
 
             // Getting the var to create a room
+            /*
             int rid = 669;
             int rpplcnt = 314159276;
             string rqlt = "******************";
             string rsize = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXL";
-
+           
             // Getting the var to create a client
-            string cname = "JEANPIERRETREBERT";
-            string cpassword = "LIFYVEGZ76873IRU";
+            string cname = "Mirnes";
+            string cpassword = "123";
 
-            dl.Add_Room(rid, rpplcnt, rqlt, rsize);
+            //dl.Add_Room(rid, rpplcnt, rqlt, rsize);
             dl.Add_Client(cname, cpassword);
 
             dl.Disp_Rooms();
@@ -309,30 +326,24 @@ namespace Hotel_California___Data_manipulation_Layer
 
             int lol = dl.Login(cname, cpassword);
             Console.WriteLine(lol);
-            Console.ReadKey();
-
             dl.Del_Room(666);
-            dl.Del_Client(cname, cpassword);
+            //dl.Del_Client(cname, cpassword);
+            dl.Disp_Rooms();
             */
+            // Getting the var to create a client
+            string cname = "Mirnes";
+            string cpassword = "123";
+            dl.Add_Client(cname, cpassword);
+            dl.Add_Room(100, 2, "***", "S");
+            DateTime begins = new DateTime(2021, 2, 10);
+            DateTime ends = new DateTime(2021, 2, 17);
+            dl.Add_Reservation(100, cname, cpassword, begins, ends);
+            dl.Add_Reservation(101, cname, cpassword, begins, ends);
+            dl.Add_Reservation(1000, cname, cpassword, begins, ends);
+            dl.Disp_Reservations();
+
             Console.ReadKey();
         }
-        /*
-            // Database context
-            Entities_oui dc = new Entities_oui();
-
-            // Getting all of the tables "imported" to collections on the C# code
-            DbSet<Rooms> Rooms = dc.Rooms;
-            DbSet<Clients> Clients = dc.Clients;
-            DbSet<Booked_Rooms> Booked_Rooms = dc.Booked_Rooms;
-            DbSet<Tasks> Tasks = dc.Tasks;
-            DbSet<Task_Type> Task_Type = dc.Task_Type;
-            DbSet<Post_Type> Post_Type = dc.Post_Type;
-
-            // -------------------------------------------------------------------------------------------------------
-
-                             
-        */
-        
     }
 }
 
