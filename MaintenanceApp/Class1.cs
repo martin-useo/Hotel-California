190c1ef4493a295e293c81660b0a41f359c38932
Martí <Mart@DESKTOP-SA1RMAA>
2021-03-04 12:22:08 +0100

    universal app local

---
+++ MaintenanceApp/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintenanceApp
+{
+    public class Task
+    {
+        public int Room_ID { get; set; }
+        public int Task_ID { get; set; }
+        public string Status { get; set; }
+    }
+    public class Task_Type
+    {
+        public int Task_ID { get; set; }
+        public int Post_ID { get; set; }
+        public string Type { get; set; }
+    }
+}
